006f1e2d8216c8cda034e898e828674c39b85a2e
Stevie <xxlennygamerxx@gmail.com>
2020-05-12 20:44:04 -0700

    Dirt Pickaxe. Finally.

---
+++ Items/SpectraItem.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SpectraMod.Items
+{
+    public class SpectraItem : GlobalItem
+    {
+        public static bool dirtPick;
+        public override void HoldItem(Item item, Player player)
+        {
+            if (item.type == ModContent.ItemType<Tools.Sets.Dirt.DirtPickaxe>())
+                dirtPick = true;
+            else
+                dirtPick = false;
+        }
+    }
+}
